4ab37a8820a9920224bfe6ad204b125d3160db25
ADAM TENG <adam.teng.2021@scis.smu.edu.sg>
2022-10-29 20:54:12 +0800

    update 2

--- script.js
+++ script.js
@@ -9,4 +9,30 @@ $(function () {
     }, 3000); 
   });
 
+function loginSwap() {
+    loginHTML = `    <div class="middle container justify-content-center">
+                        <div class="col-md-6">
+                            <h4 class="text-header text-white">Sign in to your account</h4>
+                            <form>
+                                <div class="form-row" style="border-bottom: 1px solid var(--pri)">
+                                    <input type="email" placeholder="Username or email address" class="form-control bg-transparent text-white text-wrap fw-bold">
+                                </div>
 
+                                <div class="form-row" style="border-bottom: 1px solid var(--pri)">
+                                    <input type="password" placeholder="Password" class="form-control bg-transparent text-white fw-bold">
+                                </div>
+
+                                <div class="form-row">
+                                    <button type="button" class="btn-login">Login</button>
+                                </div>
+
+                                <a class="fw-bold" href="#">Forgot Password?</a>
+                                <p class="fw-bold text-white">Don't have an account? <a href="#">Register here</a></p>
+                            </form>
+                        </div>
+                    </div>
+                `
+    section = document.getElementById('loginSection')
+    section.innerHTML = loginHTML
+    return
+}
