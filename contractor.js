166d75f1669c8f74f5e03146bf1260c16b284393
SEAN TAN <sean.tan.2021@scis.smu.edu.sg>
2022-10-30 23:15:57 +0800

    painter sample

---
+++ contractor.js
@@ -0,0 +1,109 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
+const app = Vue.createApp({
+
+    // Data Properties
+    data() {
+        return {
+
+            // sort_categories:[
+            //     "Ratings: High to Low",
+            //     "Ratings: Low to High",
+            //     "Average Delivery Time",
+            //     "Average Price"
+            // ],
+
+            painting_filters:[
+                "HDB",
+                "Condominium",
+                "Landed",
+                "Commercial"
+            ],
+
+            // table_headers: [
+            //     "Destination",
+            //     "Photo",
+            //     "Description",
+            //     "Cost (SGD)",
+            //     "Add to Itinerary"
+            // ],
+
+            painter_information: {
+                "Ah Beng Services": [
+                    "images/contractorpic1.jpg",
+                    "Fast",
+                    "Responsive",
+                    `<i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-half pri-color"></i>`,
+                    "HDB,Condominium,Landed"
+                ],
+                "Beng Huat Painting": [
+                    "images/contractorpic1.jpg",
+                    "Fast",
+                    "Responsive",
+                    `<i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>`,
+                    "HDB,Condominium,Landed,Commercial"
+                ],
+                "Seng Choo Pte Ltd": [
+                    "images/contractorpic1.jpg",
+                    "Moderate",
+                    "Slow",
+                    `<i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star"></i>
+                    <i class="bi bi-star"></i>`,
+                    "HDB,Condominium"
+                ],
+                "HomeID Painting Co.": [
+                    "images/contractorpic1.jpg",
+                    "Moderate",
+                    "Slow",
+                    `<i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star-fill pri-color"></i>
+                    <i class="bi bi-star"></i>`,
+                    "HDB,Condominium,Commercial"
+                ],
+            },
+
+            selected_filters: []
+
+        }
+    },
+
+    methods: {
+       
+
+    }
+})
+
+app.mount(".main")
