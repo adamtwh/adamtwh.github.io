cda060715c959d3e99dee25e1fd826d8c43920ca
JARON CHAN <jaronchan123@gmail.com>
2022-10-31 11:18:33 +0800

    service description

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $(function () {
     count = 0; 
     wordsArray = ["painters", "plumbers", "electricians", "movers", "contractors", "technicians"]; 
     setInterval(function () { 
-      count++; 
-      $("#tagline").fadeOut(1000, function () { 
+        count++; 
+        $("#tagline").fadeOut(1000, function () { 
         $(this).text(wordsArray[count % wordsArray.length]).fadeIn(1000); 
-      }); 
+        }); 
     }, 3000); 
-  });
+});
 
 function loginSwap() {
     loginHTML = `    <div class="middle container justify-content-center">
