4616caf58e03504a4546b59bbf30c55ccb689f52
JARON CHAN <jaronchan123@gmail.com>
2022-11-03 08:26:31 +0800

    footer

--- script.js
+++ script.js
@@ -41,3 +41,31 @@ function loginSwap() {
     section.innerHTML = loginHTML
     return
 }
+
+// ----for contact form-----
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwizvOy5kTk8d4CNuqCH5VRZ4L70N7MmCoSt7aqiAjsfso5lhYKh6VdywkXDQ4AP0AVYQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function() {
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
+let contact_popup = document.getElementById("submit-popup");
+
+function openContactPopup() {
+    contact_popup.classList.add("open-contact-popup");
+}
+
+function closeContactPopup() {
+    contact_popup.classList.remove("open-contact-popup");
+}
