f0b0ed877c6ed790b6c4c88b6fb053d1cdec505b
JARON CHAN <jaronchan123@gmail.com>
2022-11-13 14:51:26 +0800

    recaptcha on all pages

--- components/booking/booking_contractor.js
+++ components/booking/booking_contractor.js
@@ -27,27 +27,23 @@ const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
 
 form.addEventListener('submit', e => {
-e.preventDefault()
-fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        msg.innerHTML = "Message sent successfully"
-        setTimeout(function() {
-            msg.innerHTML = ""
-        }, 5000)
-        form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
+e.preventDefault();
+
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LfGFQIjAAAAAJm-kH4OKEImTo3Brw-jdGPQmiwH', {action: 'submit'}).then(function(token) {
+            // Add your logic to submit to your backend server here.
+            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => {
+                    msg.innerHTML = "Message sent successfully"
+                    setTimeout(function() {
+                        msg.innerHTML = ""
+                    }, 5000)
+                    form.reset()
+                })
+                .catch(error => console.error('Error!', error.message))
+        });
+    });
 })
-
-let contact_popup = document.getElementById("submit-popup");
-
-function openContactPopup() {
-    contact_popup.classList.add("open-contact-popup");
-}
-
-function closeContactPopup() {
-    contact_popup.classList.remove("open-contact-popup");
-}
 
 
 // for dynamic rendering
--- components/profile/register_contractor.js
+++ components/profile/register_contractor.js
@@ -110,3 +110,27 @@ function updateEmail() {
 //         console.log(checked.value)
 //     }
 // }
+
+// ----for contact form-----
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwizvOy5kTk8d4CNuqCH5VRZ4L70N7MmCoSt7aqiAjsfso5lhYKh6VdywkXDQ4AP0AVYQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+e.preventDefault();
+
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LfGFQIjAAAAAJm-kH4OKEImTo3Brw-jdGPQmiwH', {action: 'submit'}).then(function(token) {
+            // Add your logic to submit to your backend server here.
+            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => {
+                    msg.innerHTML = "Message sent successfully"
+                    setTimeout(function() {
+                        msg.innerHTML = ""
+                    }, 5000)
+                    form.reset()
+                })
+                .catch(error => console.error('Error!', error.message))
+        });
+    });
+})
