822f9d7803a115aab5f98a11357c2bf33edf6c2b
JARON CHAN <jaronchan123@gmail.com>
2022-11-13 12:33:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -252,7 +252,6 @@ e.preventDefault();
     grecaptcha.ready(function() {
         grecaptcha.execute('6LfGFQIjAAAAAJm-kH4OKEImTo3Brw-jdGPQmiwH', {action: 'submit'}).then(function(token) {
             // Add your logic to submit to your backend server here.
-            console.log("hey!")
             fetch(scriptURL, { method: 'POST', body: new FormData(form)})
                 .then(response => {
                     msg.innerHTML = "Message sent successfully"
